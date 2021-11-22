2c1efc3305536bdf4970e40b0caeed7fabb0858d
huangtiancai <1812869775@aa.com>
2021-11-22 11:37:01 +0800

    update

---
+++ HTML&CSS/4骨架屏/README.md
@@ -0,0 +1,4 @@
+skeleton 骨架屏
+- demo
+- byh5
+- bg列表
---
+++ HTML&CSS/4骨架屏/Skeleton/skeleton.js
@@ -0,0 +1,23 @@
+let sleleton = document.getElementsByClassName('card')[1]
+console.log(sleleton)
+
+// 加载数据前
+setTimeout(() => {
+  let image = document.getElementsByClassName('image')[1]
+  let h = document.querySelectorAll('h4')[1]
+  let d = document.querySelectorAll('.description')[1]
+
+  // 加载完数据
+  let node = document.createElement("img")
+  node.src = 'https://blog.huangtiancai.cn/upload/imgs/cover/cover3.jpg'
+  // 追加图片
+  image.appendChild(node);
+
+  // 标题
+  h.innerText = 'Skeleton'
+  // 正文
+  d.innerText = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex quasi enim facere commodi omnis...'
+
+  // 移除loading
+  sleleton.classList.remove('loading')
+},3000)
